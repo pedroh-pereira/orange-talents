f16178d3dba0f2fe81d807f092009c249ad9a383
Pedro Henrique <pehenri22@gmail.com>
2020-12-23 21:41:58 -0300

    Initial project

---
+++ src/main/java/br/com/zup/demo/models/Client.java
@@ -0,0 +1,24 @@
+package br.com.zup.demo.models;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Client {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String name;
+  private String email;
+  private String cpf;
+}
